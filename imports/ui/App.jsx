848f35453c94c4a08a926c0f39da2960adfba9c9
Ben Sharp <bfwsharp@gmail.com>
2016-09-25 17:20:57 +0100

    wrap new task form to only show when logged in

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -69,14 +69,15 @@ class App extends Component {
 
           <AccountsUIWrapper />
 
-          {/*  add an input field for users to add tasks to the list */}
-          <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
-            <input
-              type="text"
-              ref="textInput"
-              placeholder="Type to add new tasks"
-            />
-          </form>
+          { this.props.currentUser ?
+            <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+              <input
+                type="text"
+                ref="textInput"
+                placeholder="Type to add new tasks"
+              />
+            </form> : ''
+          }
         </header>
 
         <ul>
@@ -90,6 +91,7 @@ class App extends Component {
 App.propTypes = {
   tasks: PropTypes.array.isRequired,
   incompleteCount: PropTypes.number.isRequired,
+  currentUser: PropTypes.object,
 };
 
 export default createContainer(() => {
